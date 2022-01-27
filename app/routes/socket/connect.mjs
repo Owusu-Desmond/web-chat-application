465881df371122b78e6c418246d6513091caa797
Owusu-Desmond <desmond0406114009@gmail.com>
2022-01-27 12:18:47 +0000

    broadcasting event

--- app/routes/socket/connect.mjs
+++ app/routes/socket/connect.mjs
@@ -1,11 +1,18 @@
 import io from "../../loaders/socket.mjs";
 
+var client = 0;
 //Whenever someone connects this gets executed
 io.on('connection', socket => {
-    console.log('An user connected');
+    console.log('A user connected');
+    // send the new user a welcome message and update the other clients about him/her joining
+    client++;
+     
+    socket.emit('newclientconnect', {discription : "Hey welcome"})
+    socket.broadcast.emit('newclientconnect', {discription : client + " users connected"})
 
     //Whenever someone disconnects this piece of code executed
     socket.on('disconnect', () => {
-        console.log('A user disconnected');
+        client--;
+        socket.broadcast.emit('newclientconnect', {discription : client + " users connected"})
     });
-});
+}); 

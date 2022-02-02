91d3bb1e96f226cf4cd197e5c9e6377c9c4e3e65
Owusu-Desmond <desmond0406114009@gmail.com>
2022-02-02 11:17:49 +0000

    styling small and creating sign up and login system

--- app/routes/socket/connect.mjs
+++ app/routes/socket/connect.mjs
@@ -1,30 +1,74 @@
 import io from "../../loaders/socket.mjs";
 
-// Connecting users
-let users = [];
-// Whenever someone connects this gets executed
+let users = [{userName : 'Admin', password : 'admin'}];
+//Whenever someone connects this gets executed
 io.on('connection', socket => {
-    // Current user
-    let user;
-
-    // Set username event
-    socket.on('setUsername', data => {
-        if (users.indexOf(data) > -1) 
-            // User exists
-            socket.emit('userExists', `"${data}" is 
-                taken, please try another user name.`)
-        else {
-            // Set current user to data
-            user = data;
-            // Push to user list
-            users.push(data);
-            // Set user
-            socket.emit('setUser', data);
+    console.log('A user connected');
+    // sign up user
+    socket.on('signUp', (data) => { 
+        // validation
+        if(data.userName.length < 1){
+            return socket.emit('emtyUsername', 'Username field is emty');
         }
+        if(data.password.length < 8){
+            return socket.emit('passwordStrength', 'Password length should be more than 7');
+        }
+        // change the first character of username to upper case
+        data.userName = data.userName[0].toUpperCase() + data.userName.slice(1);
+        console.log(data.userName);
+        // loop through users stored data and check if user exit
+        for(let i of users){
+            // if username exit then username has already taken
+            if(i.userName === data.userName){
+                // user exit
+                socket.emit('userExits', `${data.userName} user name is 
+                taken try another name.`)
+                  
+            }else{
+                // if username dont exit that is when the loop ends
+                if(i == users[users.length - 1]){
+                    // store user data
+                    users.push(data);
+                    // set user
+                    socket.emit('setUser' , {username : data});
+                    console.log(users); 
+                }
+            }
+
+        }
+
+    });
+    // login user
+    socket.on('login', (data) => { 
+        // change the first character of username to upper case
+        data.userName = data.userName[0].toUpperCase() + data.userName.slice(1);
+        // loop through users stored data
+        for(let i of users){
+            // check if username exit
+            if(i.userName === data.userName){
+                console.log('username exit');
+                console.log(data);
+                // if username exit check if password match with username
+                if(i.userName === data.userName && i.password === data.password){
+                    // set user 
+                socket.emit('setUser' , {username : data.userName});
+                   
+                } else{
+                    // user password is incorrect
+                    socket.emit('userPasswordIncorrect', `user name ${data.userName} and password do not match`);
+                    return;
+                }     
+                
+            }else{
+                // if username dont exit that is when the loop ends
+                if(i == users[users.length - 1]){
+                    console.log('username dont exit');
+                    socket.emit('userDontExit', `${data.userName} username dont exit.`)
+                }
+            }
+
+        }
+
     });
 
-    // If user disconnects
-    socket.on("disconnect", () => 
-        users = users.filter(v => v !== user)
-    )
 }); 
